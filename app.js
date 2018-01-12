e2ac59ee6800ac619e3fee7bfe0c72cb188c37aa
stefan <lhy.higher@icloud.com>
2018-01-12 14:53:07 -0500

    0.000001

---
+++ app.js
@@ -0,0 +1,12 @@
+(function () {
+    //Init Firebase
+    var config = {
+        apiKey: "AIzaSyDY4kMTAHDfsRc5fQvdwegTfmav8ETKyNc",
+        authDomain: "fir-trial-93665.firebaseapp.com",
+        databaseURL: "https://fir-trial-93665.firebaseio.com",
+        projectId: "fir-trial-93665",
+        storageBucket: "fir-trial-93665.appspot.com",
+        messagingSenderId: "89227837674"
+    };
+    firebase.initializeApp(config);
+}());
